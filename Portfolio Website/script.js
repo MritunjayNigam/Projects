c7a13a4adead605e25202bfff093f3cd089abec0
MritunjayNigam <75441696+MritunjayNigam@users.noreply.github.com>
2025-03-07 12:53:12 +0530

    done

--- Portfolio Website/script.js
+++ Portfolio Website/script.js
@@ -1,9 +1,49 @@
 document.querySelector(".cross").addEventListener("click", () =>{
     document.querySelector(".slidebar").classList.add("hide")
-    document.querySelector(".ham").style.display="block"
+    if (document.querySelector(".slidebar").classList.contains("hide")) {
+        document.querySelector(".ham").style.display = "block"
+    } else {
+        document.querySelector(".ham").style.display = "none"
+    }
 })
 
 document.querySelector(".ham").addEventListener("click", ()=>{
     document.querySelector(".slidebar").classList.remove("hide")
-    document.querySelector(".ham").style.display="none"
+    if (document.querySelector(".slidebar").classList.contains("hide")) {
+        document.querySelector(".ham").style.display = "block"
+    } else {
+        document.querySelector(".ham").style.display = "none"
+    }
 })
+
+function hideHam(){
+    const ham = document.querySelector(".ham")
+    const slidebar = document.querySelector(".slidebar")
+    if(slidebar.classList.contains("hide")){
+        ham.style.display="block"
+    }
+    else{
+        ham.style.display="none"
+    }
+}
+hideHam()
+window.addEventListener("resize", hideHam)
+
+
+
+function widthNav(){
+    const element = document.querySelector(".slidebar")
+    const ham = document.querySelector(".ham");
+
+    if(window.innerWidth <= 768){
+        element.classList.add("hide")
+        ham.style.display="block"
+    }
+    else{
+        element.classList.remove("hide")
+    }
+}
+widthNav()
+window.addEventListener('resize', widthNav);// whenever someone changes width or height of window, run widthNav function
+
+
