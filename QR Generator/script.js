491902cf55aedb239e172597f441e19da51b39c5
MritunjayNigam <75441696+MritunjayNigam@users.noreply.github.com>
2025-04-16 13:09:50 +0530

    ok

---
+++ QR Generator/script.js
@@ -0,0 +1,77 @@
+//DECLARATION
+let image = document.querySelector(".container img");
+let box = document.querySelector(".container");
+let refresh = document.querySelector(".fa-rotate-right");
+let target = document.querySelector(".container input");
+
+//REFRESH
+refresh.addEventListener("click", () => {
+
+    refresh.classList.add("rotate")
+    setTimeout(() => {
+        refresh.classList.remove("rotate")
+    }, 500);
+
+    if (window.innerWidth <= 425) {
+        box.style.height = "150px"
+    } else {
+        box.style.height = "144px"
+    }
+
+    image.style.display = "none";
+
+    target.value = "";
+
+})
+
+
+//MAIN
+async function genereteQR() {
+    let target = document.querySelector(".container input").value;
+    if (target == "") {
+        alert("Box Khali Na Chordo!")
+        return
+    }
+
+    let url = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${target}`
+
+    image.src = url
+
+    async function setHeight() {
+        await new Promise(resolve => {
+            if (window.innerWidth <= 425) {
+                box.style.height = "380px"
+            } else {
+                box.style.height = "374px"
+            }
+            setTimeout(resolve, 500)
+        });
+    }
+    await setHeight()
+
+    image.style.display = "block"
+}
+
+
+//CALLING
+document.querySelector(".btn").addEventListener("click", () => {
+    genereteQR()
+})
+target.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        genereteQR()
+    }
+})
+
+//MOUSE DOWN
+document.querySelector(".btn").addEventListener("mousedown", (e) => {
+    let b = document.querySelector(".btn");
+    b.style.background ="black";
+    b.style.color ="#ccda4f"; 
+});
+document.querySelector(".btn").addEventListener("mouseup", (e) => {
+    let b = document.querySelector(".btn");
+    b.style.background ="#313896";
+    b.style.color ="white"; 
+});
+
--- test/script.js
+++
@@ -1,5 +0,0 @@
-function playMovie(url) {
-    let frame = document.getElementById("movieFrame");
-    frame.src = url;
-    frame.style.display = "block";
-}
